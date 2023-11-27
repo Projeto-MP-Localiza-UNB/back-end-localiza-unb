658ef9bfc6c04d022dbf7e14037a51780fae6bbd
Herisson Nobre <herisson.carvalho96@gmail.com>
2023-11-27 12:36:10 -0300

    Refatoração: altera nome do diretório de testes

--- tests/index.spec.js
+++ tests/index.spec.js
@@ -3,8 +3,10 @@ import app from "../index.js";
 
 describe("Teste do estado do servidor", () => {
     it("Testa se o servidor está recebendo solicitações e respondendo", async () => {
+        // Dispara uma requisição no caminho '/' para a aplicação e armazena a resposta em variável
         const res = await request(app).get("/");
 
+        // Testa se a propriedade status da resposta é 200 - caso de sucesso
         expect(res.status).toBe(200);
     });
 });
